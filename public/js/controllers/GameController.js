3fa182fa58a7d33c8b267ef14141ab2a948aaa6c
Igor Fedorov <ifedor@ymail.com>
2016-09-22 14:51:13 +0300

    game controller

---
+++ public/js/controllers/GameController.js
@@ -0,0 +1,11 @@
+/**
+ * Created by ifedor on 22.09.16.
+ */
+angular
+    .module('TickTackToeApp')
+    .controller('GameController', function($scope) {
+
+        $scope.test = 'tst 999';
+
+
+    });
