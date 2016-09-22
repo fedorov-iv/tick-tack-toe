609510bc1049fd85f9cafd92e5e587542c3460f8
Igor Fedorov <ifedor@ymail.com>
2016-09-22 20:04:56 +0300

    check win

--- public/js/controllers/GameController.js
+++ public/js/controllers/GameController.js
@@ -7,6 +7,10 @@ angular
 
         $scope.field = [null, null, null, null, null, null, null, null, null]; // field
         $scope.gameOver = false; // state
+        var winner = {
+            ai: 'Компьютер',
+            user: 'Пользователь'
+        };
 
         console.log('Initial field:', $scope.field);
 
@@ -19,6 +23,7 @@ angular
 
             $scope.field[position] = 1;
             console.log('User\'s move :', $scope.field);
+            checkWin();
             findEmptyCell();
 
         };
@@ -39,6 +44,7 @@ angular
         function moveBack(position) {
             $scope.field[position] = 0;
             console.log('Computer\'s move :', $scope.field);
+            checkWin();
         }
 
         /**
@@ -57,7 +63,7 @@ angular
 
         /**
          * Checks field for empty cells
-         * @returns {bool}
+         * @returns {boolean}
          */
         function hasEmptyCells() {
 
@@ -67,6 +73,92 @@ angular
                 }
             }
             return false;
+
+        }
+
+        function checkWin(){
+           if ($scope.field[0] !== null &&  $scope.field[1]!== null && $scope.field[2] !== null){ // проверяем первый горизонтальный ряд
+               if($scope.field[0] + $scope.field[1] + $scope.field[2] == 0){
+                   $scope.winner = winner.ai;
+                   resume();
+               }
+               if($scope.field[0] + $scope.field[1] + $scope.field[2] == 3){
+                   $scope.winner = winner.user;
+                   resume();
+               }
+
+           } else if($scope.field[3] !== null &&  $scope.field[4]!== null && $scope.field[5] !== null){ // проверяем второй горизонтальный ряд
+               if($scope.field[3] + $scope.field[4] + $scope.field[5] == 0){
+                   $scope.winner = winner.ai;
+                   resume();
+               }
+               if($scope.field[3] + $scope.field[4] + $scope.field[5] == 3){
+                   $scope.winner = winner.user;
+                   resume();
+               }
+
+           } else if($scope.field[6] !== null &&  $scope.field[7]!== null && $scope.field[8] !== null){ // проверяем третий горизонтальный ряд
+               if($scope.field[6] + $scope.field[7] + $scope.field[8] == 0){
+                   $scope.winner = winner.ai;
+                   resume();
+               }
+               if($scope.field[6] + $scope.field[7] + $scope.field[8] == 3){
+                   $scope.winner = winner.user;
+                   resume();
+               }
+
+           } else if($scope.field[0] !== null &&  $scope.field[3]!== null && $scope.field[6] !== null){ // проверяем первый вертикальный ряд
+               if($scope.field[0] + $scope.field[3] + $scope.field[6] == 0){
+                   $scope.winner = winner.ai;
+                   resume();
+               }
+               if($scope.field[0] + $scope.field[3] + $scope.field[6] == 3){
+                   $scope.winner = winner.user;
+                   resume();
+               }
+
+           } else if($scope.field[1] !== null &&  $scope.field[4]!== null && $scope.field[7] !== null){ // проверяем второй вертикальный ряд
+               if($scope.field[1] + $scope.field[4] + $scope.field[7] == 0){
+                   $scope.winner = winner.ai;
+                   resume();
+               }
+               if($scope.field[1] + $scope.field[4] + $scope.field[7] == 3){
+                   $scope.winner = winner.user;
+                   resume();
+               }
+
+           } else if($scope.field[2] !== null &&  $scope.field[5]!== null && $scope.field[8] !== null){ // проверяем третий вертикальный ряд
+               if($scope.field[2] + $scope.field[5] + $scope.field[8] == 0){
+                   $scope.winner = winner.ai;
+                   resume();
+               }
+               if($scope.field[2] + $scope.field[5] + $scope.field[8] == 3){
+                   $scope.winner = winner.user;
+                   resume();
+               }
+
+           } else if($scope.field[0] !== null &&  $scope.field[4]!== null && $scope.field[8] !== null){ // проверяем диагональ левый верхний - правый нижний
+               if($scope.field[0] + $scope.field[4] + $scope.field[8] == 0){
+                   $scope.winner = winner.ai;
+                   resume();
+               }
+               if($scope.field[0] + $scope.field[4] + $scope.field[8] == 3){
+                   $scope.winner = winner.user;
+                   resume();
+               }
+
+           } else if($scope.field[2] !== null &&  $scope.field[4]!== null && $scope.field[6] !== null){ // проверяем диагональ правый верхний - левый нижний
+               if($scope.field[2] + $scope.field[4] + $scope.field[6] == 0){
+                   $scope.winner = winner.ai;
+                   resume();
+               }
+               if($scope.field[2] + $scope.field[4] + $scope.field[6] == 3){
+                   $scope.winner = winner.user;
+                   resume();
+               }
+
+           }
+
 
         }
 
