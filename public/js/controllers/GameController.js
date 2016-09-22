a5d87d13a8f2abdfbb2e1c5777d09994aa838b7c
Igor Fedorov <ifedor@ymail.com>
2016-09-22 15:41:31 +0300

    user's and ai's moves

--- public/js/controllers/GameController.js
+++ public/js/controllers/GameController.js
@@ -3,9 +3,63 @@
  */
 angular
     .module('TickTackToeApp')
-    .controller('GameController', function($scope) {
+    .controller('GameController', function ($scope) {
 
-        $scope.test = 'tst 999';
+
+        // field
+        $scope.field = [null, null, null, null, null, null, null, null, null];
+        $scope.gameOver = false;
+
+        console.log('Initial field:', $scope.field);
+
+
+        // user's move
+        $scope.move = function (position) {
+
+            $scope.field[position] = 1;
+            console.log('User\'s move :', $scope.field);
+            findEmptyCell();
+
+        };
+
+        // start new game
+        $scope.start = function () {
+            $scope.field = [null, null, null, null, null, null, null, null, null];
+            $scope.gameOver = false;
+        };
+
+
+        //computer's move
+        function moveBack(position) {
+            $scope.field[position] = 0;
+            console.log('Computer\'s move :', $scope.field);
+        }
+
+        function findEmptyCell() {
+            if(hasEmptyCells()){
+                var position = Math.floor(Math.random() * 9);
+                $scope.field[position] !== null  ? findEmptyCell() : moveBack(position);
+            }else{
+                resume();
+            }
+
+
+        }
+        // checks field for empty cells
+        function hasEmptyCells(){
+
+            for(var i = 0; i < $scope.field.length; i++){
+                if($scope.field[i] === null){
+                    return true;
+                }
+            }
+            return false;
+
+        }
+
+        function resume(){
+            $scope.gameOver = true;
+        }
 
 
     });
